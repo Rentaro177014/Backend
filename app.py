01a826f9c97542ddb6e0ffb0a68f999840c71986
Garbulatov <javagod95@gmail.com>
2026-05-21 17:13:01 +0300

    task

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask,jsonify
+
+from toDO.routes import task_bp
 
 app = Flask(__name__)
+
+
+app.register_blueprint(task_bp,url_prefix='/tasks')
 
 
 @app.route('/')
 def main():
     return 'главная страница'
+
+
+@app.route('/posts')
+def postes():
+    return 
+
+
+
+
+@app.route('/add_post')
+def add_post():
+    return
+
+
+
+@app.route('/add_post/<id>')
+def view_id():
+    return id
 
 
 if __name__ == '__main__':
--- toDO/routes.py
+++ toDO/routes.py
@@ -1,11 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 
 
 task_bp = Blueprint('tasks',__name__,template_folder='templates')
 
+tasks_db = [
+    {'id':1,'title':'купить хлеб','description':'успеть до закрытия'},
+    {'id':2,'title':'сесть за комп','description':'успеть до того как уснешь'},
+    {'id':3,'title':'поиграть в игру','description':'как нибуьдбь'},
+    {'id':4,'title':'покушать','description':'если проглодался'},
+]
 
-@task_bp.route('/all_tasks')
+
+@task_bp.route('/')
 def get_all_tasks():
-    return 'Все задачи!'
+    return render_template('tasks.html', tasks_db=tasks_db)
 
+
+@task_bp.route('/task/<int:id>')
+def show_task(id):
+    task_one = []
+
+    for task in tasks_db:
+        if task.get('id') == id:
+            task_one.append(task)
+
+    return render_template('detail.html',task_one=task_one)
